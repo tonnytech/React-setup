331ca81eb11e8c5f753eb0d31ddc43c940cf4ad8
teitonny <tonnytei4@gmail.com>
2023-03-22 15:35:46 +0300

    make corrections

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <p className="App">when you focus on progress, not perfection </p>
+    <p className="App">Focus on progress, not perfection </p>
   );
 }
 
